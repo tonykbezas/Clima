110b82d0725e32a4774af3bf13ad288fff73ef10
tonykbezas <46576227+tonykbezas@users.noreply.github.com>
2020-01-29 23:59:58 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const axios = require('axios');
-// const clima = require('./controlador/clima');
+
+const ubicacion = require('./controlador/ubicacion');
+const clima = require('./controlador/clima');
+
 const argv = require('yargs').options({
-    nombre:{
+    nombre: {
         alias: 'n',
-        desc: 'Nombre de ka ciudad para obtener el clima',
+        desc: 'Nombre de la ciudad para obtener el clima',
         demand: true
     }
 }).argv;
 
-const info = require('./controlador/info');
-// const ubicacion = require('./controlador/ubicacion');
-//  ubicacion.getCiudadLatLon(argv.nombre)
-//      .then(console.log);
-// clima.getClima(-0.19,-78.5)
+// ubicacion.getCiudadLatLon(argv.nombre)
 //     .then(console.log);
-info.getInfo(argv.nombre)
-    .then(console.log);
+
+// clima.getClima(-0.19, -78.5)
+//     .then(console.log);
+
+const getInfo = async(ciudad) => {
+    try {
+        const coords = await ubicacion.getCiudadLatLon(ciudad);
+        const temp = await clima.getClima(coords.lat, coords.lon);
+        return `El clima de ${ coords.name } es de ${ temp }.`;
+    } catch (e) {
+        return `No se pudo determinar el clima de ${ ciudad } ${e}`;
+    }
+}
+
+getInfo(argv.nombre)
+    .then(console.log)
+    .catch(console.log);
